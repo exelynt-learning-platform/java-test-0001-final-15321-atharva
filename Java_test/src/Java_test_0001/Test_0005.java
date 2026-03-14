ab82dca9314c9e6a9530de90660f70266137b062
AtharvaMadkeDev <atharvamadke22@gmail.com>
2026-03-14 14:12:04 +0530

    Java codes

---
+++ Java_test/src/Java_test_0001/Test_0005.java
@@ -0,0 +1,21 @@
+package Java_test_0001;
+
+public class Test_0005 {
+
+	public static void main(String[] args) {
+		
+		int n=1;
+		
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print(" "+n);
+				n++;
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
